ee1a1dda978d4a9620874e719b2db86e080c0e65
Manjil <shresthamanjil21@gmail.com>
2020-08-20 17:49:43 +0200

    starting DAG

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
-# Apache Airflow demonstration
+# Apache Airflow demonstration (Python 3.8)
 
-## 
+## Airflow initialization
+```
+$ export AIRFLOW_HOME='pwd' apache_airflow
+$ airflow initdb
+
+-- airflow.cfg - consists of initial settings required for airflow (also db connection)
+
+$ airflow webserver (starts a webserver and webpage with examples and DAGs)
+
+-- Set load_examples = False in airflow.cfg to not see the example DAGs
+```
---
+++ dags/my_simple_dag.py
@@ -0,0 +1,40 @@
+
+from datetime import datetime
+
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
+
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2018, 9, 24, 10, 00, 00),
+    'concurrency': 1,
+    'retries': 0
+}
+
+
+def greet():
+    print('Writing in file...')
+
+    with open('../greet.txt', 'a+', encoding='utf-8') as f:
+        now = datetime.now()
+        t = now.strftime('%Y-%m-%d %H:%M')
+        f.write(f'{t}\n')
+
+    return 'Greeted'
+
+
+def respond():
+    return 'Greet responded again...'
+
+
+with DAG('my_simple_dag',
+         default_args=default_args,
+         schedule_interval='*/10 * * * *') as dag:
+    opr_hello = BashOperator(task_id='say Hi', bash_command='echo "Hi!!!"')
+    opr_greet = PythonOperator(task_id='greet', python_callable=greet)
+    opr_sleep = BashOperator(task_id='sleep_me', bash_command='sleep 5')
+    opr_respond = PythonOperator(task_id='respond', python_callable=respond)
+
+opr_hello >> opr_greet >> opr_sleep >> opr_respond
